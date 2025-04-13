6e06351b07e4517cde92e784cf2f8f035191b4a3
Leslee7789 <lesliefleischer8@gmail.com>
2025-04-13 16:07:42 -0700

    create elelemts

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 import {countries} from "./countries.js";
 
-console.log(countries);
 
-// const gridContainer = document.getElementById("grid-country-container");
-// gridContainer.className = "grid-country-container";
+
+ const gridContainer = document.getElementById("grid-country-container");
+ gridContainer.className = "grid-country-container";
+ let countryContainer = document.createElement("div");
+ countryContainer.className = "country";
+ gridContainer.appendChild(countryContainer);
+
+ let flagImage = document.createElement("div");
+ flagImage.className = "flag";
+ countryContainer.appendChild(flagImage);
+
+ let image = document.createElement("img");
+ image.src = countries[0].flag;
+ flagImage.appendChild(image);
+
+ let countryName = document.createElement("p");
+countryName.className = "country-name";
+
+countryName.textContent = countries[0].name;
+countryContainer.appendChild(countryName);
